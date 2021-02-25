bf0eab79ed4a2cd459f51266729e2d187e4a1c7d
William Cheausky <williamchenausky@yahoo.com>
2021-02-25 14:15:09 -0500

    add questions for manager

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ const Employee = require('./lib/employee')
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
+const Choice = require('inquirer/lib/objects/choice')
 
 const welome = "Welcome to my team building website. Here you're able to enter all of your employees information into a document. Ready when you are!"
 
+function promptMan() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the managers name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the managers ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the managers email?',
+
+        },
+        {
+            type: 'list',
+            name: 'Add next',
+            choices: [ 'Manger', 'Engineer', 'Intern', 'Exit']
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is the managers suite number?',
+        },
+    ])
+}
