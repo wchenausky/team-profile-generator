5d62115d58e3a8a9748dcd30c03ff859e0c3f6fe
William Cheausky <williamchenausky@yahoo.com>
2021-02-26 13:49:50 -0500

    update employee.test

---
+++ _test_/employee.test.js
@@ -0,0 +1,13 @@
+const employee = require('../lib/employee')
+
+describe('employee', () => {
+    describe('Intialization', () => {
+    it('it should return an object containing a "job" property of Employee', () => {
+        const empObj = new employee('Bob', 10, 'Bob@yahoo.com');
+
+        expect(empObj.name).toEqual('Bob')
+        expect(empObj.id).toEqual('10')
+        expect(empObj.email).toEqual('Bob@yahpp.com')
+    })
+})
+});
